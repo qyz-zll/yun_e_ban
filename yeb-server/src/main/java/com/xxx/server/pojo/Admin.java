21d5f32fafe4a11714fe6d717a51401bdcdd6754
zll <2023305043@qq.com>
2022-11-18 16:55:56 +0800

    denglu

--- yeb-server/src/main/java/com/xxx/server/pojo/Admin.java
+++ yeb-server/src/main/java/com/xxx/server/pojo/Admin.java
@@ -8,8 +8,11 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serializable;
+import java.util.Collection;
 
 /**
  * <p>
@@ -24,7 +27,7 @@ import java.io.Serializable;
 @Accessors(chain = true)
 @TableName("t_admin")
 @ApiModel(value="Admin对象", description="")
-public class Admin implements Serializable {
+public class Admin implements Serializable, UserDetails {
 
     private static final long serialVersionUID = 1L;
 
@@ -60,4 +63,28 @@ public class Admin implements Serializable {
     private String remark;
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
 }
